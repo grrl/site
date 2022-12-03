011447427ebd57424702b3f5e09986cdb4ecc87a
your name <you@example.com>
2022-12-03 17:48:50 +0200

    uwu

--- home.php
+++ home.php
@@ -32,8 +32,10 @@ if(!isset($_SESSION['username'])){
   <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav center">
+        <!--
         <li><a href="login.php">LogIN</a></li>
         <li><a href="register.php">SignUp</a></li>
+        -->
         <li><a href="logout.php">LogOut</a></li>
       </ul>
 
