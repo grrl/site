d15a283f7180ff285536d79dd97fef4826e708d2
myname <email>
2023-02-13 16:32:30 +0200

    .

--- plinko.js
+++ plinko.js
@@ -18,6 +18,7 @@ var redraw = function(){
     
   requestAnimationFrame(redraw);
 }
+//canvas size is 
 function move(){
   firstx+=5;
   //var x = firstx;//Math.random() // this returns a float between 0.0 and 1.0
@@ -28,40 +29,4 @@ redraw();
 
 setInterval(move, 1000);
 
-/*
-const ctx = canvas.getContext('2d');
-
- const p = {
-   x: 25,
-   y: 25
- };
- const velo = 3;
- const corner = 50;
- const rad = 20;
- 
- const ball = {
-   x: p.x,
-   y: p.y
- };
- 
- let moveX = Math.cos(Math.PI / 180 * corner) * velo;
- let moveY = Math.sin(Math.PI / 180 * corner) * velo;
-
-const DrawMe = () => {
-   ctx.clearRect(0, 0, 400, 300);
-
-   if (ball.x > canvas.width - rad || ball.x < rad) moveX = -moveX;
-   if (ball.y > canvas.height - rad || ball.y < rad) moveY = -moveY;
-
-   ball.x += moveX;
-   ball.y += moveY;
-
-   ctx.beginPath();
-   ctx.fillStyle = 'green';
-   ctx.arc(ball.x, ball.y, rad, 0, Math.PI * 2, false);
-   ctx.fill();
-   ctx.closePath();
-}
-//setInterval(DrawMe, 10);
-*/
 });
