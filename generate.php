fddeba4749b51a6a53cde27db0b9eafc3c8935ff
myname <email>
2023-02-18 19:11:49 +0200

    .

--- generate.php
+++ generate.php
@@ -248,14 +248,14 @@ $sql = "UPDATE plinko SET coinin=coinin +" . $bet_value . ", coinout=coinout +".
 ", opal=opal +" . $bet_value * 0.01 * 0.1 . ", ruby=ruby +". $bet_value * 0.01 * 0.1 .", emerald=emerald +" .
 $bet_value * 0.01 * 0.2 . ", sapphire=sapphire +" . $bet_value * 0.01 * 0.2 . ", diamond=diamond +" . $bet_value * 0.01 * 0.4;
 */
-/*
+
 $sql = "UPDATE plinko SET coinin=coinin +" . $bet_value. "coinout=coinout +". $win_amount . ", cycle=cycle +" . 1 .
 ", opal=opal +" . $opal_multiplier . ", ruby=ruby +". $ruby_multiplier .", emerald=emerald +" .
 $emerald_multiplier . ", sapphire=sapphire +" . $sapphire_multiplier . ", diamond=diamond +" . $diamond_multiplier;
-*/
 
-$sql = "UPDATE plinko SET coinin=coinin +" . $bet_value . ", coinout=coinout +" . $win_amount . ", cycle=cycle +" . 1 .
-", opal=opal +" . $opal_multiplier;
+
+//$sql = "UPDATE plinko SET coinin=coinin +" . $bet_value . ", coinout=coinout +" . $win_amount . ", cycle=cycle +" . 1 .
+//", opal=opal +" . $opal_multiplier;
 
 if ($conn->query($sql) === TRUE) {
   //echo "New record created successfully";
